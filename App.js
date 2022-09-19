835a35a61a8e842fa95dae72f69e4b5a7edeac9c
David <davidrek2008@hotmail.com>
2022-09-19 10:41:20 -0300

    Correção 1- fechamento da view

--- App.js
+++ App.js
@@ -66,7 +66,7 @@ export default function App() {
       <View style={styles.results}>
         <Text style={styles.historyText}>{lastNumber}</Text>
         <Text style={styles.resultText}>{currentNumber}</Text>
-      <View>
+      </View>
 
       {/* Area onde os botões são exibidos*/}
       <View style={styles.buttons}>
@@ -83,7 +83,6 @@ export default function App() {
         )}
       </View>
     </View>
-    
   );
 }
 
